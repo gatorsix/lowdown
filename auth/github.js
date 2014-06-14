c2c4923c91d2418b52cdbcb83345055fbec4ae13
Derek Lindahl <dlindahl@customink.com>
2014-06-13 23:18:45 -0700

    Add initial GitHub OAuth integration

---
+++ auth/github.js
@@ -0,0 +1,17 @@
+var passport = require('koa-passport');
+var GitHubStrategy = require('passport-github').Strategy;
+
+var opts = {
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: 'http://localhost:3000/auth/github/callback',
+  scope: 'user:email, repo:status, read:org'
+};
+
+passport.use(new GitHubStrategy(opts, function(accessToken, refreshToken, profile, done) {
+  console.log('[GitHub] accessToken: %s refreshToken:%s', accessToken, refreshToken);
+  return done(null, profile);
+  // User.findOrCreate({ githubId: profile.id }, function (err, user) {
+  //   return done(err, user);
+  // });
+}));
--- index.js
+++ index.js
@@ -1,12 +1,58 @@
+var dotenv = require('dotenv');
+dotenv.load();
+
 var koa = require('koa');
 var serve = require('koa-static');
+var session = require('koa-sess');
 
-var app = koa();
 
-app.use(serve('public'));
+var server = koa();
+server.keys = [process.env.SECRET_KEY];
+server.use(session());
 
-app.on('error', function(err){
-  console.error('server error', err);
+var passport = require('koa-passport');
+require('./auth/github');
+server.use(passport.initialize());
+server.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
 });
 
-app.listen(3000);
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+var Router = require('koa-router');
+var app = new Router();
+
+app.get('/login', passport.authenticate('github'));
+app.get('/logout', function *() {
+  this.logout();
+  this.redirect('/');
+});
+
+app.get('/auth/github/callback', passport.authenticate('github', {
+  successRedirect: '/',
+  failureRedirect: '/fail'
+}));
+
+app.get('/fail', function *() {
+  this.body = 'ONOES! Auth has failed!';
+});
+
+app.get('/', function *(next) {
+  if(!Object.keys(this.session.passport).length) return yield next;
+  var user = this.session.passport.user;
+  this.body = 'RENDER THE APP: ' + user.displayName;
+});
+
+server.use(app.middleware());
+
+server.use(serve('public'));
+
+server.on('error', function(err){
+  console.trace(err);
+});
+
+server.listen(3000);
