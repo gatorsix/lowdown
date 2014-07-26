928c713bce646db28f1a16790d232fa4d4435fad
Derek Lindahl <dlindahl@customink.com>
2014-07-26 16:12:08 -0700

    Add initial SASS integration

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 var gutil = require('gulp-util');
 var browserify = require('browserify');
 var watchify = require('watchify');
 var source = require('vinyl-source-stream');
 
 var DEBUG = process.env.NODE_ENV === 'development';
+var paths = {
+  javascripts: './app/assets/javascripts/ui.js',
+  stylesheets: './app/assets/stylesheets/*.scss'
+};
 
 function handleError(err) {
   gutil.beep();
@@ -15,7 +20,7 @@ function handleError(err) {
  * UI application code
  */
 function uiBuilder(packager) {
-  var bundler = packager('./app/assets/javascripts/ui.js');
+  var bundler = packager(paths.javascripts);
 
   function rebundle(files) {
     bundler
@@ -41,5 +46,23 @@ gulp.task('watch-ui', function() {
   uiBuilder(watchify);
 });
 
-gulp.task('build', ['build-ui']);
-gulp.task('watch', ['watch-ui']);
+/**
+ * UI CSS
+ */
+gulp.task('build-sass', function () {
+  gulp.src(paths.stylesheets)
+    .on('error', handleError)
+    .pipe(sass({
+      errLogToConsole: true,
+      sourceMap: 'sass',
+      sourceComments: 'map'
+    }))
+    .pipe(gulp.dest('./app/assets/stylesheets'));
+});
+
+gulp.task('watch-sass', function() {
+  gulp.watch(paths.stylesheets, ['build-sass']);
+});
+
+gulp.task('build', ['build-ui', 'build-sass']);
+gulp.task('watch', ['watch-ui', 'watch-sass']);
